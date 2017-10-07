d2f15ba105c2088691c757cfb0907bde48b68975
sajjukhoja <sajjadalikhoja@gmail.com>
2017-10-07 00:31:06 -0400

    database

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get 'tasks' => 'pages#about'
 
-  recources :catalogues
+  resources :catalogues
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
